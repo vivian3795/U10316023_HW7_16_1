ec5254f2c25cf5a5e55d20eb50f8848662cad3d9
vivian3795 <vivian3795@gmail.com>
2015-04-30 00:45:54 +0800

    Update crads.java
    plus refresh button
    write a pick four cards method

--- crads.java
+++ crads.java
@@ -6,12 +6,20 @@
  */
  
  import javax.swing.*;
+ import java.awt.event.*;
  import java.awt.*;
  import java.util.Random;
  
 public class cards extends JFrame{
 	//create an imageicon array
 	ImageIcon[] arr = new ImageIcon[52];
+	int r1,r2,r3,r4;
+	
+	//create a button called Ran
+	JPanel Ran = new JPanel();
+	//create a button called Re
+	JPanel Re = new JPanel();	
+	JButton refresh = new JButton("Refresh");
 	
 	//constructor
 	public cards(){
@@ -19,13 +27,29 @@ public class cards extends JFrame{
 		for(int c = 1 ; c <= 52 ; c++){
 			arr[c-1] = new ImageIcon("card/"+c+".png");
 		}
-		
+			
 		//set layout
-		setLayout(new GridLayout(1,5,5,5));
+		Ran.setLayout(new GridLayout(1,4));
+		//pick four cards
+		PickCards();
+		Ran.add(new JLabel(arr[r1]));
+		Ran.add(new JLabel(arr[r2]));
+		Ran.add(new JLabel(arr[r3]));
+		Ran.add(new JLabel(arr[r4]));
+		
+		//create a button called Refresh
+		Re.add(refresh);
+		
+		//add to frame
+		add(Ran,BorderLayout.NORTH);
+		add(Re,BorderLayout.SOUTH);
 
+		refresh.addActionListener(new FreshListener());
+	}
+	
+	void PickCards(){
 		//random four cards
 		Random ran = new Random();
-		int r1,r2,r3,r4;
 		do{
 		r1 = ran.nextInt(52);
 		r2 = ran.nextInt(52);
@@ -33,12 +57,21 @@ public class cards extends JFrame{
 		r4 = ran.nextInt(52);
 		}while((r1 == r2) || (r1 == r3) || (r1 == r4)
 		|| (r3 == r2) || (r4 == r2) || (r3 == r4));
-		add(new JLabel(arr[r1]));
-		add(new JLabel(arr[r2]));
-		add(new JLabel(arr[r3]));
-		add(new JLabel(arr[r4]));
 	}
 
+	private class FreshListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e){
+			Ran.remove();
+			Container repaint;
+			PickCards();
+			Ran.add(new JLabel(arr[r1]));
+			Ran.add(new JLabel(arr[r2]));
+			Ran.add(new JLabel(arr[r3]));
+			Ran.add(new JLabel(arr[r4]));
+		}
+	}
+	
 	//main method
 	public static void main(String[] args) {
 		//create cards object 
@@ -46,7 +79,7 @@ public class cards extends JFrame{
 		//set title
 		CARDS.setTitle("Pick Four Cards");
 		//set size
-		CARDS.setSize(400,150);
+		CARDS.setSize(300,170);
 		//center the frame
 		CARDS.setLocationRelativeTo(null);
 		//terminate when the frame is closed
