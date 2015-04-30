29f8b4b48585bf22e3ec610324f6281e6222c0d6
vivian3795 <vivian3795@gmail.com>
2015-04-30 11:40:00 +0800

    Update crads.java
    use javax.swing.JLabel - public void setIcon(Icon icon)
    to set and reset icon

--- crads.java
+++ crads.java
@@ -13,11 +13,14 @@
 public class cards extends JFrame{
 	//create an imageicon array
 	ImageIcon[] arr = new ImageIcon[52];
-	int r1,r2,r3,r4;
 	
-	//create a button called Ran
+	//create a JPanel called Ran and four cards label
 	JPanel Ran = new JPanel();
-	//create a button called Re
+	JLabel card1 = new JLabel();
+	JLabel card2 = new JLabel();
+	JLabel card3 = new JLabel();
+	JLabel card4 = new JLabel();
+	//create a JPanel called Re and refresh button
 	JPanel Re = new JPanel();	
 	JButton refresh = new JButton("Refresh");
 	
@@ -25,6 +28,8 @@ public class cards extends JFrame{
 	public cards(){
 		//set cards into array
 		for(int c = 1 ; c <= 52 ; c++){
+			//put all of cards in file card and named them 1-52.png 
+			//arr[0] store 1.png, arr[1] store 2.png, and so on
 			arr[c-1] = new ImageIcon("card/"+c+".png");
 		}
 			
@@ -32,10 +37,10 @@ public class cards extends JFrame{
 		Ran.setLayout(new GridLayout(1,4));
 		//pick four cards
 		PickCards();
-		Ran.add(new JLabel(arr[r1]));
-		Ran.add(new JLabel(arr[r2]));
-		Ran.add(new JLabel(arr[r3]));
-		Ran.add(new JLabel(arr[r4]));
+		Ran.add(card1);
+		Ran.add(card2);	
+		Ran.add(card3);	
+		Ran.add(card4);	
 		
 		//create a button called Refresh
 		Re.add(refresh);
@@ -44,10 +49,14 @@ public class cards extends JFrame{
 		add(Ran,BorderLayout.NORTH);
 		add(Re,BorderLayout.SOUTH);
 
+		//register listener
 		refresh.addActionListener(new FreshListener());
 	}
 	
 	void PickCards(){
+		//four cards number named r1,r2,r3,r4
+		int r1,r2,r3,r4;
+		
 		//random four cards
 		Random ran = new Random();
 		do{
@@ -55,20 +64,23 @@ public class cards extends JFrame{
 		r2 = ran.nextInt(52);
 		r3 = ran.nextInt(52);
 		r4 = ran.nextInt(52);
-		}while((r1 == r2) || (r1 == r3) || (r1 == r4)
-		|| (r3 == r2) || (r4 == r2) || (r3 == r4));
+		}//check four cards if duplicate, redo
+		while((r1 == r2) || (r1 == r3) || (r1 == r4)
+		|| (r3 == r2) || (r4 == r2) || 	(r3 == r4));
+		
+		//set four cards label's icon  
+		card1.setIcon(arr[r1]);
+		card2.setIcon(arr[r2]);
+		card3.setIcon(arr[r3]);
+		card4.setIcon(arr[r4]);
 	}
 
+	//listener class
 	private class FreshListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e){
-			Ran.remove();
-			Container repaint;
+			//refresh four cards
 			PickCards();
-			Ran.add(new JLabel(arr[r1]));
-			Ran.add(new JLabel(arr[r2]));
-			Ran.add(new JLabel(arr[r3]));
-			Ran.add(new JLabel(arr[r4]));
 		}
 	}
 	
